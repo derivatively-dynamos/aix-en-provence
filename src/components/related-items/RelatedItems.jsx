8b368886dae66cf15039a066d3a8c2d399a78d42
Tyler Leet <tylerleet254@gmail.com>
2022-08-24 17:39:30 -0400

    complete framework for RIC

--- src/components/related-items/RelatedItems.jsx
+++ src/components/related-items/RelatedItems.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+import RelatedProducts from './carousels/RelatedProducts.jsx';
+import YourOutfit from './carousels/YourOutfit.jsx';
 
 const RelatedItems = () => {
 
   return (
-    <div>
-      Here's some 2 div
-    </div>
+    <RICDiv>
+      <RelatedProducts />
+      <YourOutfit />
+    </RICDiv>
   )
 }
+
+const RICDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 0.5em;
+  background-color: #7e7e7e;
+`
 
 export default RelatedItems;
