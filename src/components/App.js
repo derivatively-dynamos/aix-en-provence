2c8a790da736675b59e9d1e16be4c9cb91969e23
gdeedler <grantdeedler@gmail.com>
2022-08-23 20:15:31 -0400

    Replace 'style' with 'styled' components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
 import './app.scss';
+import styled from 'styled-components'
+
+const Button = styled.button`
+  border: 2px red dashed;
+  color: darkred;
+  font-size: 2em;`
+
 
 class App extends Component {
   render() {
     return (
       <div className='header'>
         <h1>Hello World!</h1>
+        <Button>Click me!</Button>
       </div>
     )
   }
