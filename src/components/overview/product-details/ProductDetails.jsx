1e97eb47b25b6116533cf95d98ddc1c12be8743b
gdeedler <grantdeedler@gmail.com>
2022-08-24 19:50:43 -0400

    Create and import dummy product data

--- src/components/overview/product-details/ProductDetails.jsx
+++ src/components/overview/product-details/ProductDetails.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import AddToCart from "./AddToCart";
 import ProductInformation from "./ProductInformation";
 import StyleSelector from "./StyleSelector";
 import InfoList from "./InfoList";
 
-const ProductDetails = () => (
-  <Container>
-    <ProductInformation/>
-    <StyleSelector/>
-    <AddToCart />
-    <InfoList />
-  </Container>
-)
+import product from "./oneproduct.json";
+
+const ProductDetails = () => {
+
+  useEffect(() => {
+    console.log(product);
+  }, [])
+
+  return (
+    <Container>
+      <ProductInformation/>
+      <StyleSelector/>
+      <AddToCart />
+      <InfoList />
+    </Container>
+  )
+}
 
 const Container = styled.section`
   display: flex;
