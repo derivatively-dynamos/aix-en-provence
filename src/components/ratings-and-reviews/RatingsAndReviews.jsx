9df3ebce673963dc12955446831eb6caca3f173f
Brian <itscrousie@gmail.com>
2022-08-24 18:58:07 -0400

    Create necessary files

--- src/components/ratings-and-reviews/RatingsAndReviews.jsx
+++ src/components/ratings-and-reviews/RatingsAndReviews.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const RatingsAndReviews = () => {
 
   return (
     <div>
-      Here's some 4 div
+      <h2>RATINGS AND REVIEWS</h2>
+      <Container>
+        <BdContainer></BdContainer>
+        <RlContainer></RlContainer>
+      </Container>
     </div>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 0.5em;
+  background-color: gray;
+`
+const BdContainer = styled(Container)`
+  background-color: red;
+`
+const RlContainer = styled(Container)`
+  background-color: purple;
+`
 
 export default RatingsAndReviews;
