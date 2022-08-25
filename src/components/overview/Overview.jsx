3908a47a2f6b5f1d9d6ed08b91d074110a3f17e6
gdeedler <grantdeedler@gmail.com>
2022-08-25 09:57:26 -0400

    Move state out to overview

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import ImageGallery from './image-gallery/ImageGallery';
 import ProductDetails from './product-details/ProductDetails';
+import axios from 'axios';
+
+import exproduct from './product-details/examples/exampleproduct.json';
+import exstyles from './product-details/examples/examplestyles.json';
+
+const PRODUCT_ID = 37312;
 
 const Overview = () => {
+
+  const [product, setProduct] = useState(exproduct);
+  const [styles, setStyles] = useState(exstyles);
+  const [style, setStyle] = useState(exstyles.results[0]);
 
   return (
     <Container>
       <ImageGallery/>
-      <ProductDetails/>
+      <ProductDetails
+        product={product}
+        styles={styles}
+        style={style}/>
     </Container>
   )
 }
--- src/components/overview/product-details/ProductDetails.jsx
+++ src/components/overview/product-details/ProductDetails.jsx
@@ -4,35 +4,9 @@ import AddToCart from './AddToCart';
 import ProductInformation from './ProductInformation';
 import StyleSelector from './StyleSelector';
 import InfoList from './InfoList';
-import axios from 'axios';
-import { GIT_AUTH, API_URL } from '../../../../config';
 
-import exproduct from './examples/exampleproduct.json';
-import exstyles from './examples/examplestyles.json';
+const ProductDetails = ({product, styles, style}) => {
 
-const PRODUCT_ID = 37312;
-console.log(API_URL);
-
-const ProductDetails = () => {
-
-  const [product, setProduct] = useState(exproduct);
-  const [styles, setStyles] = useState(exstyles);
-  const [style, setStyle] = useState(exstyles.results[0]);
-
-  // useEffect(() => {
-  //   axios
-  //     .get(`${API_URL}/products/${PRODUCT_ID}`, {
-  //       headers: {
-  //         Authorization: GIT_AUTH,
-  //       },
-  //     })
-  //     .then((response) => {
-  //       console.log(response.data);
-  //       setProduct(response.data);
-  //     })
-  //     .catch(err => console.log(err))
-  // },
-  // []);
 
   return (
     <Container>
