86f5e2682fc6e38b591c64d2b131fc7bf92e07c2
gdeedler <grantdeedler@gmail.com>
2022-08-24 21:11:40 -0400

    Create sale price functionality

--- src/components/overview/product-details/ProductDetails.jsx
+++ src/components/overview/product-details/ProductDetails.jsx
@@ -1,27 +1,48 @@
-import React, { useEffect } from "react";
-import styled from "styled-components";
-import AddToCart from "./AddToCart";
-import ProductInformation from "./ProductInformation";
-import StyleSelector from "./StyleSelector";
-import InfoList from "./InfoList";
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import AddToCart from './AddToCart';
+import ProductInformation from './ProductInformation';
+import StyleSelector from './StyleSelector';
+import InfoList from './InfoList';
+import axios from 'axios';
+import { GIT_AUTH, API_URL } from '../../../../config';
 
-import product from "./oneproduct.json";
+import exproduct from './examples/exampleproduct.json';
+import exstyles from './examples/examplestyles.json';
+
+const PRODUCT_ID = 37312;
+console.log(API_URL);
 
 const ProductDetails = () => {
 
-  useEffect(() => {
-    console.log(product);
-  }, [])
+  const [product, setProduct] = useState(exproduct);
+  const [styles, setStyles] = useState(exstyles);
+  const [style, setStyle] = useState(exstyles.results[0]);
+
+  // useEffect(() => {
+  //   axios
+  //     .get(`${API_URL}/products/${PRODUCT_ID}`, {
+  //       headers: {
+  //         Authorization: GIT_AUTH,
+  //       },
+  //     })
+  //     .then((response) => {
+  //       console.log(response.data);
+  //       setProduct(response.data);
+  //     })
+  //     .catch(err => console.log(err))
+  // },
+  // []);
 
   return (
     <Container>
-      <ProductInformation/>
-      <StyleSelector/>
+      <ProductInformation product={product} style={style} />
+      <StyleSelector />
       <AddToCart />
       <InfoList />
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.section`
   display: flex;
@@ -29,9 +50,8 @@ const Container = styled.section`
   flex: 1;
   background-color: #313131;
   color: lightgray;
-  padding: .5em;
+  padding: 0.5em;
   border: 1px solid cyan;
-`
-
+`;
 
 export default ProductDetails;
--- src/components/overview/product-details/ProductInformation.jsx
+++ src/components/overview/product-details/ProductInformation.jsx
@@ -3,17 +3,35 @@ import styled from "styled-components";
 import Row from "../../shared-components/Row";
 import Col from "../../shared-components/Col";
 
-const ProductInformation = () => (
+const ProductInformation = ({product: {category, name, description}, style:{original_price, sale_price}}) => (
   <Col>
     <Row>
       <div>★★★☆☆</div>
       <div><a href="">Read all reviews</a></div>
     </Row>
-    <H4>CATEGORY</H4>
-    <H2>Expanded Product Name</H2>
-    <H4>$369</H4>
+    <H4>{category}</H4>
+    <H2>{name}</H2>
+    <H4>
+      <ProductPrice
+        original_price={original_price}
+        sale_price={sale_price}/>
+    </H4>
   </Col>
 )
+
+const ProductPrice = ({original_price, sale_price}) => {
+
+  let display = <div>${original_price}</div>;
+  if (sale_price) {
+    display = <div>
+    <del>${original_price}</del> <Sale>${sale_price}</Sale>
+  </div>
+  }
+
+  return(
+    display
+  )
+}
 
 const H4 = styled.h4`
   margin: 1em 0 0 .5em;
@@ -22,6 +40,8 @@ const H2 = styled.h2`
   margin-top: 5px;
   margin-bottom: 0px;
 `
-
+const Sale = styled.span`
+  color: red;
+`
 
 export default ProductInformation;
