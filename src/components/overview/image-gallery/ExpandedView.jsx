f1b365d4862f1f6beabc2606456d0273d240df78
gdeedler <grantdeedler@gmail.com>
2022-08-26 14:50:49 -0400

    Create early zoom and pan functionality

--- src/components/overview/image-gallery/ExpandedView.jsx
+++ src/components/overview/image-gallery/ExpandedView.jsx
@@ -1,36 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 
 const ExpandedView = ({ photo, setPopover }) => {
 
-  const [xOff, setXOff] = useState(0);
-  let xOffset = 0;
-  let yOffset = 0;
-  let parentRect = null;
-  const logPos = (e) => {
-    let rect = e.target.getBoundingClientRect();
-    parentRect = e.target.parentElement.getBoundingClientRect();
-    let x = e.clientX - rect.left;
-    xOffset = x / parentRect.width * rect.width;
-    let y = e.clientY - rect.top;
-    yOffset = y / parentRect.height * rect.height;
-    setXOff(xOffset);
-    console.log(xOffset);
+  const imageCont = useRef(null);
+  const image = useRef(null);
+  const [xOffset, setXOffset] = useState(0);
+  const [yOffset, setYOffset] = useState(0);
+
+  const handleMouseMove = (e) => {
+    let rect = imageCont.current.getBoundingClientRect()
+    let imageRect = image.current;
+    console.log(image.current);
+    let hiddenX = imageRect.width - rect.width;
+    let hiddenY = imageRect.height - rect.height;
+    let xPct = (e.clientX - rect.left) / rect.width;
+    let yPct = (e.clientY - rect.top) / rect.height;
+    let newXOffset = -(xPct * hiddenX);
+    let newYOffset = -(yPct * hiddenY);
+
+    setXOffset(newXOffset);
+    setYOffset(newYOffset);
+    // var setnewHeight = -(mouseoffTopPercent * excessHeight);
   }
+
   return (
-    <ImageContainer onMouseMove={logPos}>
-      <ExpandedImage src={photo} onClick={() => setPopover(false)} xOffset={`-${xOff}px`}/>
+    <ImageContainer ref={imageCont} onMouseMove={handleMouseMove} >
+      <ExpandedImage
+        src={photo}
+        onClick={() => setPopover(false)}
+        style={{transform: `translate(${xOffset}px, ${yOffset}px)`}}
+        ref={image}
+      />
     </ImageContainer>
-  ) ;
+  );
 };
 
-const ExpandedImage = styled.img.attrs(props => ({
-  left: props.xOffset;
-}))`
-  width: 200%;
-  position: relative;
-  top:-0%;
+const ExpandedImage = styled.img`
+  display:block;
+  width: 3000px;
+  transition-timing-function: linear;
 `;
+
 const ImageContainer = styled.div`
   top: 0;
   left: 0;
@@ -39,6 +50,6 @@ const ImageContainer = styled.div`
   flex: 1;
   position: absolute;
   overflow: hidden;
-`
+`;
 
 export default ExpandedView;
