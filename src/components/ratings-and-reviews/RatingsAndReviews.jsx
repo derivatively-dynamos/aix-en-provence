ee6dfebe21a1851151480dc0e48d2f74698e7e2f
Brian <itscrousie@gmail.com>
2022-08-26 11:59:26 -0400

    Render Reviews in ReviewList

--- src/components/ratings-and-reviews/RatingsAndReviews.jsx
+++ src/components/ratings-and-reviews/RatingsAndReviews.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import ReviewBreakdown from './ReviewBreakdown.jsx'
 import ReviewList from './ReviewList.jsx'
 
+//sample data:
+import sampleProduct from "./sample.js"
+
 const RatingsAndReviews = () => {
+
+  const [ products ] = useState(sampleProduct);
 
   return (
     <AppContainer>
       <h2>RATINGS AND REVIEWS</h2>
       <Container>
         <ReviewBreakdown />
-        <ReviewList />
+        <ReviewList products={products}/>
       </Container>
     </AppContainer>
   )
@@ -20,7 +26,6 @@ const Container = styled.div`
   display: flex;
   width: 100%;
   padding: 0.5em;
-  background-color: gray;
 `
 const AppContainer = styled(Container)`
   flex-direction: column;
