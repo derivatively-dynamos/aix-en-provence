24460a887e6fb2f3afe3694f3a1baf1cfbb5d28c
James Anderson <jamespanderson22@gmail.com>
2022-08-26 15:21:58 -0400

    "get requests working, question/answer functionality in progress"

--- src/components/overview/image-gallery/DefaultView.jsx
+++ src/components/overview/image-gallery/DefaultView.jsx
@@ -4,24 +4,27 @@ import ThumbnailList from "./ThumbnailList";
 
 const DefaultView = () => (
   <Container>
-    <MainImage src="https://via.placeholder.com/300"/>
-    <ThumbnailList/>
-    <P>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem, fugit quasi. Ex itaque nam ad saepe, quis omnis, corrupti repellendus, quae minima labore repellat nisi molestias magnam dignissimos nobis. Voluptas.</P>
+    <MainImage src="https://via.placeholder.com/300" />
+    <ThumbnailList />
+    <P>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem,
+      fugit quasi. Ex itaque nam ad saepe, quis omnis, corrupti repellendus,
+      quae minima labore repellat nisi molestias magnam dignissimos nobis.
+      Voluptas.
+    </P>
   </Container>
-)
+);
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
   position: relative;
-`
+`;
 const MainImage = styled.img`
   border: 2px solid cyan;
   flex: 1;
-`
-const P = styled.p`
-
-`
+`;
+const P = styled.p``;
 
 export default DefaultView;
