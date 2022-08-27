4f3208146da2e30168b1ab68fe1e5cb7321ec67d
gdeedler <grantdeedler@gmail.com>
2022-08-26 20:05:54 -0400

    Comment out axios requests

--- src/components/questions-and-answers/QuestionsAndAnswers.jsx
+++ src/components/questions-and-answers/QuestionsAndAnswers.jsx
@@ -3,21 +3,21 @@ import styled from "styled-components";
 import SearchBar from "./SearchBar";
 import QuestionComponent from "./Questions-Answers/q-a-container/QuestionComponent";
 import QuestionForm from "./Questions-Answers/QuestionForm/QuestionForm";
-import API from "./env/config";
+// import API from "./env/config";
 
 const QuestionsAndAnswers = () => {
   const [questions, setQuestions] = useState([]);
 
-  useEffect(() => {
-    API.get("/questions?product_id=37312")
-      .then(({ data }) => data.results)
-      .then((results) => {
-        setQuestions(results);
-      })
-      .catch((err) => {
-        console.log("ERROR", err);
-      });
-  }, []);
+  // useEffect(() => {
+  //   API.get("/questions?product_id=37312")
+  //     .then(({ data }) => data.results)
+  //     .then((results) => {
+  //       setQuestions(results);
+  //     })
+  //     .catch((err) => {
+  //       console.log("ERROR", err);
+  //     });
+  // }, []);
   // will include parent state passed as props referencing product ID
 
   // if array is greater than 1, display up to 5 questions
