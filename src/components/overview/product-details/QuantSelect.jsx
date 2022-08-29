3b86de7f585a2519ed47068306b3cb4f7c6924fa
gdeedler <grantdeedler@gmail.com>
2022-08-29 12:30:30 -0400

    Select quantity and control state

--- src/components/overview/product-details/QuantSelect.jsx
+++ src/components/overview/product-details/QuantSelect.jsx
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-const QuantSelect = ({ quant }) => {
+const QuantSelect = ({ quant, setQuant, maxQuant, sku }) => {
+
+  const [inStock, setInStock] = useState(true);
+  useEffect(() => {
+    if(sku) {
+      if(maxQuant === 0) {
+        setInStock(false);
+      }
+    } else {
+      setInStock(true);
+    }
+  }, [quant, sku])
+  const quants = [];
+  for (let i = 1; i <= maxQuant && i <= 15; i++) {
+    quants.push(
+      <Option key={i} value={i}>
+        {i}
+      </Option>
+    );
+  }
+
   return (
-    <Select>
-      <option>Quantity</option>
-    </Select>
+    <Container>
+      {maxQuant === 0 && sku !== null ? (
+        <OutOfStock>OUT OF STOCK</OutOfStock>
+      ) : (
+        <Select value={quant} onChange={(e) => setQuant(e.target.value)}>
+          <Option value="0">Quantity</Option>
+          {quants}
+        </Select>
+      )}
+    </Container>
   );
 };
 
+const Container = styled.div`
+  align-self: flex-start;
+  flex-basis: 50%;
+  color: #c0c0c0;
+  font-size: 14px;
+  display: flex;
+`;
 const Select = styled.select`
   border-radius: 0;
   border: 1px solid darkgray;
@@ -16,8 +50,18 @@ const Select = styled.select`
   color: #c0c0c0;
   font-size: 14px;
   padding: 10px;
-  flex-basis: 50%;
-  align-self: flex-start;
+  position: relative;
+`;
+const OutOfStock = styled.div`
+  border-radius: 0;
+  border: 1px solid darkgray;
+  background-color: #292929;
+  color: #c0c0c0;
+  font-size: 14px;
+  padding: 10px;
+`;
+const Option = styled.option`
+  width: 300px;
 `;
 
 export default QuantSelect;
