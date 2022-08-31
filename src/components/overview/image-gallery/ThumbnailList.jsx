18a81d7f7241e254bf36a1362329de4900fe3d8b
gdeedler <grantdeedler@gmail.com>
2022-08-31 10:54:11 -0400

    Handle missing icons properly

--- src/components/overview/image-gallery/ThumbnailList.jsx
+++ src/components/overview/image-gallery/ThumbnailList.jsx
@@ -1,5 +1,5 @@
-import React, { useRef } from "react";
-import styled from "styled-components";
+import React, { useRef } from 'react';
+import styled from 'styled-components';
 
 const ThumbnailList = ({ photos, setIndex, index, scrollPos }) => {
   const icons = photos.map((photo, i) => {
@@ -14,9 +14,10 @@ const ThumbnailList = ({ photos, setIndex, index, scrollPos }) => {
     );
   });
 
-  return ( //Empty div aligns scroll
+  return (
+    //Empty div aligns scroll
     <Ul ref={scrollPos}>
-      <div/>
+      <div />
       {icons}
       <BotSpace />
     </Ul>
@@ -49,7 +50,7 @@ const Icon = styled.img`
   border: none;
   scroll-snap-align: start;
   scroll-margin-top: 4px;
-  outline: ${props => props.selected ? `2px solid` : "none"};
+  outline: ${(props) => (props.selected ? `2px solid` : 'none')};
 `;
 const BotSpace = styled.div`
   width: 10px;
