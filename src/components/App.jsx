bb8d16f536874f4a7be562d640ab8c5485414e02
gdeedler <grantdeedler@gmail.com>
2022-09-02 10:56:55 -0400

    Remove API calls from overview

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -38,7 +38,7 @@ const App = () => {
     >
       <ThemeProvider theme={themes[theme]}>
         <Header curTheme={theme} setTheme={setTheme} themes={themes} />
-        <OverviewTrack productId={productId} />
+        <OverviewTrack product={product} styles={styles} />
         <SlimColumn>
           <RelatedItems productId={productId} setProductId={setProductId} />
           <QuestionsAndAnswers />
