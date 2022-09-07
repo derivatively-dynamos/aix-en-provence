ff5ef6530df8b590e6cf6144dc81a46425a0e8a4
gdeedler <grantdeedler@gmail.com>
2022-09-07 12:17:34 -0400

    Create basic server and route API through it

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,62 @@
 const express = require('express');
 const morgan = require('morgan');
+const axios = require('axios');
+require('dotenv').config();
 let app = express ();
+
+
+const API_URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe';
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    Authorization: process.env.AUTH,
+  }
+})
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname));
 
 const port = process.env.PORT || 8010;
+
+app.get('/api/*', (req, res) => {
+  const url = req.url.replace('/api/', '/')
+  api.get(url)
+    .then(response => {
+      res.status(200);
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400);
+      res.send('Error')
+    })
+})
+app.post('/api/*', (req, res) => {
+  const url = req.url.replace('/api/', '/')
+  api.post(url)
+    .then(response => {
+      res.status(200);
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400);
+      res.send('Error')
+    })
+})
+app.put('/api/*', (req, res) => {
+  const url = req.url.replace('/api/', '/')
+  api.put(url)
+    .then(response => {
+      res.status(200);
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400);
+      res.send('Error')
+    })
+})
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
--- src/components/shared-components/api.js
+++ src/components/shared-components/api.js
@@ -2,7 +2,7 @@ import {GIT_AUTH, API_URL} from '../../../config';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: '/api',
   headers: {
     Authorization: GIT_AUTH
   }
