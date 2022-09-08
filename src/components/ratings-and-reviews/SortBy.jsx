3aeda67bdab4dd99a79eab8406712ab1bb98a408
Brian <itscrousie@gmail.com>
2022-09-08 11:24:58 -0400

    Correct options background color

--- src/components/ratings-and-reviews/SortBy.jsx
+++ src/components/ratings-and-reviews/SortBy.jsx
@@ -7,9 +7,9 @@ const SortBy = ({ reviews, setSort }) => {
     <Container>
       <Inline>{reviews.length} reviews, sorted by</Inline>
       <Selector onClick={(e) => setSort(e.target.value)}>
-        <option value="relevance" defaultValue>relevance</option>
-        <option value="helpful">helpful</option>
-        <option value="newest">newest</option>
+        <Option value="relevance" defaultValue>relevance</Option>
+        <Option value="helpful">helpful</Option>
+        <Option value="newest">newest</Option>
       </Selector>
     </Container>
   )
@@ -25,7 +25,9 @@ const Inline = styled.div`
   font-size: 1.25em;
   font-weight: 600;
 `
-
+const Option = styled.option`
+  background-color: ${(props) => props.theme.background};
+`
 const Selector = styled.select`
   color: ${(props) => props.theme.color};
   outline: 0px solid transparent;
