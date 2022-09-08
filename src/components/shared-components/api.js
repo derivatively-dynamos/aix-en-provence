d25d2ca812ad038577df95ab487d30965944f346
gdeedler <grantdeedler@gmail.com>
2022-09-08 18:20:23 -0400

    Hide auth

--- src/components/shared-components/api.js
+++ src/components/shared-components/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-console.log(process.env.NODE_ENV);
 
 const url = process.env.NODE_ENV === 'production' ? '/api' : 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe';
+const auth = process.env.NODE_ENV === 'production' ? undefined : process.env.AUTH;
 
 const api = axios.create({
   baseURL: url,
   headers: {
-    Authorization: process.env.AUTH
+    Authorization: auth,
   }
 })
 api.interceptors.request.use(request => {
