a9eab1a743ba3ebee06217ab00e662e28acf6d70
gdeedler <grantdeedler@gmail.com>
2022-09-10 10:30:11 -0400

    Change port

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(morgan('dev'));
 app.use(expressStaticGzip(path.join(__dirname, '../dist')));
 app.use(compression());
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 app.get('/api/*', (req, res) => {
   const url = req.url.replace('/api/', '/')
